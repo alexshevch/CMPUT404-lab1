f75f2ce9e48943a871c059624a01181ceefdf31c
Alex Shevchenko <shevaroller@gmail.com>
2016-09-07 15:12:23 -0600

    make the script print its raw version from github

--- lab1.py
+++ lab1.py
@@ -1,4 +1,4 @@
 import requests
 
-response = requests.post('http://ccid-eddieantonio.rhcloud.com/oleksiy')
-print response.status_code
+response = requests.get('https://raw.githubusercontent.com/shevaroller/CMPUT404-lab1/master/lab1.py')
+print response.text
